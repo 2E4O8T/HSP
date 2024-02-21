9cb60bc07606bdcce8c6fb5c0c9fc128cdb88723
2E4O8T <benoit.froidevan@axa.fr>
2024-02-21 12:28:33 +0100

    Add-hmi-project-skeleton

---
+++ HMI/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HMI.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
